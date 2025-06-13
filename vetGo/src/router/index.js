ba7348684409c71e7944d01dfc227229269b0822
gabrielimarta <gabrielirocha2730@gmail.com>
2025-06-13 05:45:09 -0300

    feat: iniciando criação de homeView e NavBar

--- vetGo/src/router/index.js
+++ vetGo/src/router/index.js
@@ -1,22 +1,53 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
+import AgendamentoView from '@/views/AgendamentoView.vue'
+import CadastroView from '@/views/CadastroView.vue'
+import LoginView from '@/views/LoginView.vue'
+import ServicosView from '@/views/ServicosView.vue'
+import SobrenosView from '@/views/SobrenosView.vue'
+import VeterinariosView from '@/views/VeterinariosView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/home',
       name: 'home',
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/',
+      name:'agendamento',
+      component: AgendamentoView,
     },
+     {
+      path: '/',
+      name:'cadastro',
+      component: CadastroView,
+    },
+     {
+      path: '/',
+      name:' login',
+      component: LoginView,
+    },
+     {
+      path: '/',
+      name:'servico',
+      component: ServicosView,
+    },
+     {
+      path: '/',
+      name:'sobrenos',
+      component: SobrenosView,
+    },
+     {
+      path: '/',
+      name:'veterinario',
+      component: VeterinariosView,
+    },
+     
+   
+    
   ],
 })
 
---
+++ vetGo/src/views/CadastroView.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1> aaaa</h1>
+</template>
+
+<style scoped>
+
+</style>
+
