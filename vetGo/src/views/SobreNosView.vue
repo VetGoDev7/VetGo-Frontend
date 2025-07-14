80cb3cc18fa46582ae89ab349060cf95293b46be
gabrielimarta <gabrielirocha2730@gmail.com>
2025-07-14 08:52:10 -0300

    feat: iniciando SobreNosView

--- vetGo/src/views/SobreNosView.vue
+++ vetGo/src/views/SobreNosView.vue
@@ -1,6 +1,25 @@
+<script setup>
+import '@/assets/base.css'
+</script>
+
 <template>
-  <div>
-    <h1>teste</h1>
-    
-  </div>
+  <main class="aboutUs">
+    <section class="storys">
+      <div class="info">
+        <img src="/vetu.png" class="img-clinica">
+        <h2>ngniergoejrg</h2>
+        <p>hevbfyerfygeyrfg</p>
+      </div>
+    </section>
+    <section class="story-2">
+      <div class="info-2">
+        
+      </div>
+    </section>
+    <section class="local">
+      <div class="local-info">
+  
+      </div>
+    </section>
+  </main>
 </template>
