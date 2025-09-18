ace00b4ec7491d622b44ea9d49f2dd32c8ec7db1
gabrielimarta <gabrielirocha2730@gmail.com>
2025-09-18 18:03:01 -0300

    feat: instalação pwa

--- vetGo/vite.config.js
+++ vetGo/vite.config.js
@@ -2,13 +2,45 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-//import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config
 export default defineConfig({
   plugins: [
     vue(),
-   // vueDevTools(),
+     VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Vet GO',
+        short_name: 'VetGO',
+        description: 'Vet GO: Clínica Veterinária',
+        theme_color: '#94C38F',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: '/pet.png',
+            purpose: 'any',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: '/pet.png',
+            purpose: 'maskable',
+          },
+        ],
+        id: 'com.vet.go.app',
+        orientation: 'any',
+        background_color: '#ffffff',
+        start_url: '.',
+        launch_handler: {
+          client_mode: ['navigate-existing', 'auto'],
+        },
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
   ],
   resolve: {
     alias: {
