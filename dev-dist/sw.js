36b22f7d2ceea16bf705b6eaff5d2fa32ae830cc
Rafabitte17 <rafabitte1@gmail.com>
2025-11-03 08:54:52 -0300

    Feat: retirando Serviço no Agendamentos

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.g54h7p6u9qg"
+    "revision": "0.hsmss9hgbns"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
